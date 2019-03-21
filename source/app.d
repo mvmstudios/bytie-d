0ee1ed754a2030d364359df81bafd58e6dc60e8d
Maurice Schmidt <mvmogo@gmail.com>
2019-03-21 21:12:37 +0100

    todo

--- source/app.d
+++ source/app.d
@@ -1,7 +1,9 @@
 import bindbc.sdl;
 
+/*
+@TODO: Error handling 
+*/
 void main() {
-	// ERROR HANDLING
 	loadSDL();
 
 	SDL_Window* window = SDL_CreateWindow("Bytie-D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
